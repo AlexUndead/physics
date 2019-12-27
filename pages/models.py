ccc8f61be85d798c79b883bd4e2acb1321d872e7
anehaev <Alex_Undead_92@bk.ru>
2019-12-27 23:44:45 +0300

    first_commit

---
+++ pages/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Page(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True, db_index=True, verbose_name="Активность")
+    url = models.SlugField(unique=True, verbose_name="ЧПУ")
+    title = models.CharField(max_length=100, verbose_name="Название страницы")
+    description = models.TextField(verbose_name="Текст страницы")
