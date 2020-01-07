b9867d794c43c4d8d2d4e2632599461a208d6738
Alex <alex_undead_92@bk.ru>
2020-01-07 09:17:46 +0200

    three commit

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Page
+from django.http import Http404
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html', context={})
+
+def simple_page(request, url):
+    page = get_object_or_404(Page, url = url)
+    return HttpResponse('hello world')
