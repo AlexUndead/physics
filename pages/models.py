d00a9e9dab0f1af9feaf3aee43f0798076de8cf6
Alex <Alex_Neha_92@mail.ru>
2020-02-05 14:23:58 +0300

    startpoint

--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Category(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True, db_index=True, verbose_name="Активность")
+    name = models.CharField(max_length=100, verbose_name="Название категории")
+    url = models.SlugField(unique=True, verbose_name="ЧПУ")
+
+    def __str__(self):
+        return self.name
 
 class Page(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -8,3 +17,7 @@ class Page(models.Model):
     url = models.SlugField(unique=True, verbose_name="ЧПУ")
     title = models.CharField(max_length=100, verbose_name="Название страницы")
     description = models.TextField(verbose_name="Текст страницы")
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=1)
+
+    def __str__(self):
+        return self.title
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
-from .models import Page
+from .models import Page, Category
 from django.http import Http404
 
 def index(request):
     return render(request, 'index.html', context={})
 
 def simple_page(request, url):
-    page = get_object_or_404(Page, url = url)
+    page = get_object_or_404(Page, url = url, category=1, active=1)
     return HttpResponse('hello world')
+
+def category_page(request, category, url):
+    category = get_object_or_404(Category, url = category, active=1)
+    page = get_object_or_404(Page, url = url, category=category.id, active=1)
+    return HttpResponse('category finded')
+
