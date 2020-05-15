347eac487a079f7bbec4c72ff1ddd5588e45ffcb
Alex <Alex_Neha_92@mail.ru>
2020-05-15 17:22:14 +0300

    Подключил ckeditor

--- pages/models.py
+++ pages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class Page(models.Model):
@@ -7,7 +8,7 @@ class Page(models.Model):
     active = models.BooleanField(default=True, db_index=True, verbose_name="Активность")
     url = models.SlugField(unique=True, verbose_name="ЧПУ")
     title = models.CharField(max_length=100, verbose_name="Название страницы")
-    description = models.TextField(verbose_name="Текст страницы")
+    description = RichTextUploadingField()
 
     def __str__(self):
         return self.title
--- physics/urls.py
+++ physics/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('publications/', include('publications.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('pages.urls')),
 ]
