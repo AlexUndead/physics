16df7cfc33b0242f0b076411a2fee0e32059c5c8
Alex <Alex_Neha_92@mail.ru>
2020-05-15 16:16:09 +0300

    Удалил категории для простых страниц

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from pages.models import Page
-from pages.models import Category 
 
 admin.site.register(Page)
-admin.site.register(Category)
--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<slug:category>/<slug:url>/', views.category_page, name='category-page'),
     path('<slug:url>/', views.simple_page, name='simple-page'),
     path('', views.index, name='index'),
 ]
